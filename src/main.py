f62202e4cad0a6c7c3631732e184b861f34d7983
Illuminum2 <trost.zureden08@icloud.com>
2025-03-10 00:36:39 +0100

    Fix: Example server now also includes port

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ async def main():
     log.info("main() - MC Tracking API started")
 
     db = DBHandler()
-    if not db.servers.exists_ip("mc.hypixel.net"):
+    if not db.servers.exists_ip("mc.hypixel.net:25565"):
         db.servers.add("mc.hypixel.net", 0, 1)  # A permanent example
 
     updater = TrackingPointUpdater(UPDATE_FREQUENCY, TRACKING_RETENTION_TIME, SERVER_RETENTION_TIME)
